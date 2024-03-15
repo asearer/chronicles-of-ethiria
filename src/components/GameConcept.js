6216ff333438a09a9d58ee301506d0e377c57594
asearer <amsearer84@gmail.com>
2024-03-15 15:03:08 -0400

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   src/App.css 	modified:   src/components/GameConcept.js

--- src/components/GameConcept.js
+++ src/components/GameConcept.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './GameConcept.css';
 
 function GameConcept() {
   return (
-    <div>
+    <div className="game-concept-container">
       <h2>Game Concept</h2>
       <p>This is the initial idea and concept for the game.</p>
     </div>
